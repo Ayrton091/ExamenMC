11e7748e01c0b9a848e427757ee39ea9d630a690
Ayrton091 <riostelloayrton@gmail.com>
2022-04-16 11:50:43 -0500

    tercera actualizacion
    bueno

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/MainActivity.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/MainActivity.kt
@@ -1,33 +1,82 @@
 package com.example.teacherassistant
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.teacherassistant.databinding.ActivityMainBinding
+import java.io.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        var ValorMC : EditText = findViewById(R.id.EditMC)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-       /*
-        var EditEF : EditText = findViewById(R.id.EditEF)
-        var ValorEF: Int = EditEF.text.toString().toInt()
-
-        var EditP : EditText = findViewById(R.id.EditP)
-        var ValorP: Int = EditP.text.toString().toInt()*/
-
+        binding.btnRegistrarCurso.setOnClickListener {
+            Guardar(binding.EditNombreCurso.text.toString())
+            binding.NombreCurso.text=Cargar()
+        }
 
         val btnAdd= findViewById<Button>(R.id.btnAdd)
         btnAdd.setOnClickListener {
             val lanzar = Intent(this,MainActivity2::class.java)
-            Toast.makeText( this,"ValorMC", Toast.LENGTH_SHORT).show()
             startActivity(lanzar)
         }
+
+
+
+
+
+
     }
 
-}
+    private fun Cargar(): String {
+        var texto=" "
+        try {
+            val rutaSD = baseContext.getExternalFilesDir(null)?.absolutePath
+            val miCarpeta = File(rutaSD, "Datos")
+            val ficheroFisico=File(miCarpeta,"Datos.txt")
+            val fichero = BufferedReader(InputStreamReader(FileInputStream(ficheroFisico)))
+            val texto = fichero.use(BufferedReader::readText)
+        }
+        catch (e:Exception){
+            //nada
+        }
+        return texto
+
+    }
+
+    private fun Guardar(texto: String) {
+        try {
+            val rutaSD = baseContext.getExternalFilesDir(null)?.absolutePath
+            val miCarpeta = File(rutaSD, "Datos")
+            if (!miCarpeta.exists()) {
+                miCarpeta.mkdir()
+            }
+            val ficheroFisico = File(miCarpeta, "Datos.txt")
+            ficheroFisico.appendText("$texto\n")
+        }
+        catch (e:Exception){
+            Toast.makeText(this,"no se ha podido guardar",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+    }
+
+
+
+
+
+
--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/MainActivity3.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/MainActivity3.kt
@@ -2,10 +2,16 @@ package com.example.teacherassistant
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.teacherassistant.databinding.ActivityMain3Binding
+import com.example.teacherassistant.databinding.ActivityMainBinding
 
 class MainActivity3 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main3)
     }
+
 }
